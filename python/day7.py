2fb7fe47d0b67dd11c33297ed97f0c6d7d1289d1
MOOC <676924@users.noreply.github.com>
2021-12-07 19:29:10 +0100

    day 7, added part 2 shorter version

--- python/day7.py
+++ python/day7.py
@@ -3,6 +3,9 @@ from utils import input_from_stream
 def part1(xs):
     return min([sum([abs(x - n) for x in xs]) for n in range(0, max(xs)-min(xs))])
 
+def part2_shorter(xs):
+    return min([int(sum([abs(x - n) * (abs(x - n) + 1)/2 for x in xs])) for n in range(0, max(xs)-min(xs))])
+
 def part2(xs):
     # Moving 1 step costs 1, 2 steps cost 1+2=3, 3 steps costs 1+2+3=6, ...
     costs = [0]*max(xs)*2
@@ -18,3 +21,4 @@ xs = [int(f) for f in input_from_stream()[0].split(',')]
 
 print(f'Part 1: {part1(xs)}')
 print(f'Part 2: {part2(xs)}')
+print(f'Part 2 (shorter solution): {part2_shorter(xs)}')
